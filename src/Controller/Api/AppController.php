e2c24217d0cbb2c89113d3733e7d68bae9918bb1
euonymus <euonymus0220@gmail.com>
2018-02-01 15:30:09 +0900

    JWT setting

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -5,9 +5,11 @@ use Cake\Event\Event;
 class AppController extends Controller
 {
     use \Crud\Controller\ControllerTrait;
-    public $components = [
-        'RequestHandler',
-        'Crud.Crud' => [
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Crud.Crud', [
             'actions' => [
                 'Crud.Index',
                 'Crud.View',
@@ -20,6 +22,28 @@ class AppController extends Controller
                 'Crud.ApiPagination',
                 'Crud.ApiQueryLog'
             ]
-        ]
-    ];
+        ]);
+        $this->loadComponent('Auth', [
+            'storage' => 'Memory',
+            'authenticate' => [
+                'Form' => [
+                    'scope' => ['Users.active' => 1]
+                ],
+                'ADmad/JwtAuth.Jwt' => [
+                    'parameter' => 'token',
+                    'userModel' => 'Users',
+                    'scope' => ['Users.active' => 1],
+                    'fields' => [
+                        'username' => 'id'
+                    ],
+                    'queryDatasource' => true
+                ]
+            ],
+            'unauthorizedRedirect' => false,
+            'checkAuthIn' => 'Controller.initialize'
+        ]);
+    }
+
+
+
 }
